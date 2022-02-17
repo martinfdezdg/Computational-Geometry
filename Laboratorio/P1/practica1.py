6f576ae008824212cea2af1214f5cc584d32d659
Martín <martinfdezdg@icloud.com>
2022-02-17 10:16:33 +0100

    finished

--- Laboratorio/P1/practica1.py
+++ Laboratorio/P1/practica1.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 """
 PRÁCTICA 1: ATRACTOR LOGÍSTICO
+Belén Sánchez Centeno
 Martín Fernández de Diego
 """
 
@@ -9,7 +9,19 @@ import numpy as np
 import random as rand
 
 """
-Dados dos elementos o conjuntos de elementos y un épsilon suficientemente pequeño
+Dados una x, un cierto delta y un intervalo [a,b]
+ajusta la eps para que x+eps y x-eps quepan en dicho intervalo
+"""
+# Restando epsilon evitamos que epsilon sature en los extremos indeseablemente
+def ajustar(x,delta,a,b,epsilon=0.001):
+    if x + delta > b:
+        return b - x - epsilon
+    if x - delta < a:
+        return x - a - epsilon
+    return delta
+
+"""
+Dados dos elementos
 devuelve si son lo suficientemente parecidos
 """
 def igual(x,y,epsilon=0.001):
@@ -41,6 +53,17 @@ def orbita(x0,r,f,N):
     for i in range(N):
         orb[i] = fn(x0,r,f,i)
     return(orb)
+    
+"""
+Dado un punto x0, una r, una función f y dos enteros M y N con N > M
+devuelve una lista de f^i(xm) con 0 <= i < N y xm = f^m(x0)
+"""
+def suborbita(x0,r,f,M,N):
+    xm = fn(x0,r,f,M)
+    orb = np.empty([N-M])
+    for i in range(N-M):
+        orb[i] = fn(xm,r,f,i)
+    return(orb)
 
 """
 Dado una subórbita
@@ -58,92 +81,58 @@ def periodo(suborb):
     return(per)
 
 """
-Dada una función f y dos enteros N0 y N
-devuelve una lista ordenada con los per elementos de una subórbita final de N respecto de una órbita de N0 elementos
+Dada una función f y dos enteros N y N_cola
+devuelve una lista ordenada con los per elementos de una subórbita final de N_cola respecto de una órbita de N elementos
 """
-def atractor(x0,r,f,N0,N):
-    orb = orbita(x0,r,f,N0)
-    ult = orb[-1*np.arange(N,0,-1)]
+def atractor(x0,r,f,N,N_cola):
+    orb = orbita(x0,r,f,N)
+    ult = orb[-1*np.arange(N_cola,0,-1)]
     per = periodo(ult)
-    V0 = np.sort([ult[N-1-i] for i in range(per)])
-    return V0
+    V = np.sort([ult[N_cola-1-i] for i in range(per)])
+    return V
 
 """
-Dada una función f y dos enteros N0 y MAX_N0
-devuelve el mínimo N0 que cumpla la condición de recubrimiento del algoritmo
+Dada una función f y un entero N
+devuelve el mínimo m que cumpla la condición de recubrimiento del algoritmo
 """
-# Encontramos un N1 a partir del que se puede intuir una cuenca de atracción
-def tiempo_transitorio(x0,r,f,N0,MAX_N0):
+# Encontramos un m a partir del que se puede intuir una cuenca de atracción
+def tiempo_transitorio(x0,r,f,N):
+    m = N
     recubierto = False
-    while not recubierto and N0 < MAX_N0:
-        # Primera iteración fuera del bucle para obtener máximos de referencia
-        xn = fn(x0,r,f,N0)
-        suborb = orbita(xn,r,f,N0)
-        max_ant, min_ant = abs(np.max(suborb)), abs(np.min(suborb))
+    while not recubierto:
         # Si no cumple la condición del algoritmo, no hay recubrimiento para ese tiempo transitorio
-        for i in np.arange(1,3):
-            xn = logistica(suborb[-1],r)
-            suborb = orbita(xn,r,f,N0*2**i)
-            max_act, min_act = abs(np.max(suborb)), abs(np.min(suborb))
+        for i in range(3):
+            suborb = suborbita(x0,r,f,2**i*m,2**(2**i)*m) # m - 2m / 2m - 4m / 4m - 16m
+            Dt_act = np.max(suborb) - np.min(suborb)
             # Condición del algoritmo
-            if (max_act <= max_ant and min_act >= min_ant):
-                max_ant, min_ant = max_act, min_act
-                recubierto = True
+            if (i == 0) or (Dt_act <= Dt_ant and igual(Dt_act,Dt_ant)):
+                # Guardamos el recubrimiento anterior
+                Dt_ant = Dt_act
+                recubierto = i == 2;
             else :
                 # Aumentamos el tiempo transitorio
-                N0 *= 2
-                recubierto = False
+                m *= 2
                 break
-    return N0
+    return m
 
 """
-Dada una función f, dos enteros N0 y N que indican longitudes en la sucesión, un conjunto atractor, dos enteros que limitan la ejecución y un épsilon suficientemente pequeño
-devuelve el mayor épsilon posible que define un entorno de atracción respecto al V0
+Dada una función f, dos enteros N y N_cola que indican longitudes en la sucesión y el conjunto atractor
+devuelve el mayor delta posible que define un entorno de atracción respecto al V
 """
-def error_x(x0,r,f,N0,N,V0,MAX_N0,MAX_ERR,epsilon=0.1):
+def error_x(x0,r,f,N,N_cola,V,delta=0.5):
+    delta = ajustar(x0,delta,0,1)
     estable = False
-    while not estable and MAX_ERR > 0:
-        estable = True
-        # Comprobación de si es un punto estable
-        N1_mayor = tiempo_transitorio(min(1,x0+epsilon),r,f,N0,MAX_N0)
-        N1_menor = tiempo_transitorio(max(0,x0-epsilon),r,f,N0,MAX_N0)
-        if (N1_mayor >= MAX_N0 or N1_menor >= MAX_N0):
-            epsilon /= 2
-            MAX_ERR -= 1
-            estable = False
+    while not estable:
+        N_der = tiempo_transitorio(x0+delta,r,f,N)
+        N_izq = tiempo_transitorio(x0-delta,r,f,N)
+        V_der = atractor(x0+delta,r,f,N_der,N_cola)
+        V_izq = atractor(x0-delta,r,f,N_izq,N_cola)
+        # Comprobamos si es un punto estable
+        if not ((len(V_der) == len(V) and len(V_izq) == len(V)) and (igual(V_der,V) and igual(V_izq,V))):
+            delta /= 2
         else :
-            # Comprobación de si está cerca de una bifurcación
-            V1_mayor = atractor(abs(min(1,x0+epsilon)),r,f,N1_mayor,N)
-            V1_menor = atractor(abs(x0-epsilon),r,f,N1_menor,N)
-            if (len(V1_mayor) != len(V0) or len(V1_menor) != len(V0)):
-                epsilon /= 2
-                MAX_ERR -= 1
-                estable = False
-            # Comprobación de si es suficientemente estable
-            elif (not igual(V1_mayor,V0) or not igual(V1_menor,V0)):
-                epsilon /= 2
-                MAX_ERR -= 1
-                estable = False
-    return epsilon
-
-"""
-Dada una función f, dos enteros N0 y N que indican longitudes en la sucesión, un conjunto atractor, dos enteros que limitan la ejecución y un épsilon suficientemente pequeño
-devuelve el mayor épsilon posible que define un entorno de atracción respecto al V0
-"""
-def error_r(x0,r,f,N0,N,V0,MAX_ERR,epsilon=0.1):
-    estable = False
-    while not estable and MAX_ERR > 0:
-        estable = True
-        # Comprobación de si está cerca de una bifurcación
-        r_mayor = min(4.000,r + epsilon)
-        V1_mayor = atractor(x0,r_mayor,f,N0,N)
-        r_menor = max(3.544,r - epsilon)
-        V1_menor = atractor(x0,r_menor,f,N0,N)
-        if (len(V1_mayor) != len(V0) or len(V1_menor) != len(V0)):
-            epsilon /= 2
-            MAX_ERR -= 1
-            estable = False
-    return epsilon
+            estable = True
+    return delta
 
 
 
@@ -153,34 +142,59 @@ class Formato:
     RESET = "\033[0m"
 
 # CONSTANTES
-MAX_N0, MAX_ERR = 500, 10
-N0, N = 50, 50
-x0 = rand.uniform(0,1)
+N0, N_cola = 100, 50
+
+x0 = 0.7471248146931895
+# rand.uniform(0,1)
 
 # APARTADO i)
 print("\n" + Formato.BOLD + "Apartado i)" + Formato.RESET)
 
 i = 0
-V0_aux = np.empty([N])
 while i < 2:
     r = rand.uniform(3.000,3.544)
-    N1 = tiempo_transitorio(x0,r,logistica,N0,MAX_N0)
-    if N1 < MAX_N0:
-        V0 = atractor(x0,r,logistica,N1,N)
-        err_x = error_x(x0,r,logistica,N0,N,V0,MAX_N0,MAX_ERR)
-        if len(V0) != len(V0_aux) or not igual(V0,V0_aux):
-            print(i+1, "> Cuenca de atracción de x0 =", x0, "+-", err_x, "en r =", r)
-            print("  >", V0)
-            V0_aux = V0
-            i += 1
-
+    # Calculamos un numero de iteraciones adecuado
+    N = tiempo_transitorio(x0,r,logistica,N0)
+    # Calculamos el conjunto atractor
+    V = atractor(x0,r,logistica,N,N_cola)
+    # Si los conjuntos obtenidos son distintos
+    if i == 0 or not ((len(V) == len(V_ant)) and igual(V,V_ant)):
+        # Calculamos el posible error
+        err_x = error_x(x0,r,logistica,N,N_cola,V)
+        # Mostramos
+        print(i+1,"- Cuenca de atracción de x0 =",x0,"+-",err_x,"en r =",r)
+        print("  >",V)
+        V_ant = V
+        i += 1
+        
 # APARTADO ii)
 print("\n" + Formato.BOLD + "Apartado ii)" + Formato.RESET)
 
-V0 = np.empty([])
-while V0.size != 8:
-    r = rand.uniform(3.544,4.000)
-    V0 = atractor(x0,r,logistica,N0,N)
-err_r = error_r(x0,r,logistica,N0,N,V0,MAX_ERR)
-print("  > Hay una cuenca de atracción de 8 elementos con r =", r, "+-", err_r)
-print("  >", V0)
+ext_izq, ext_der = [], []
+err_r = 0.001
+intervalo = False
+for r in np.arange(3.544,4.000,err_r):
+    # Calculamos el conjunto atractor
+    V = atractor(x0,r,logistica,N0,N_cola)
+    # Si no estamos en el intervalo y encontramos una cuenca de 8 elementos
+    if not intervalo and len(V) == 8:
+        ext_izq.append(r)
+        intervalo = True
+    # Si estamos en el intervalo y encontramos una cuenca que no tiene 8 elementos
+    elif intervalo and len(V) != 8:
+        ext_der.append(r-err_r)
+        intervalo = False
+
+print("  -",len(ext_izq),"intervalos de cuencas de atracción de 8 elementos con una sensibilidad de",err_r,"en x0 =",x0)
+print("  >",end=' ')
+for izq,der in zip(ext_izq,ext_der):
+    print("[",izq,",",der,"]",end='')
+
+# Tomamos un valor aleatorio del intervalo
+for r in np.random.permutation(np.arange(3.544,4.000,0.001)):
+    V = atractor(x0,r,logistica,N0,N_cola)
+    if len(V) == 8:
+        err_x = error_x(x0,r,logistica,N0,N_cola,V)
+        print("\nEj: Cuenca de atracción de 8 elementos de x0 =",x0,"+-",err_x,"en r =",r)
+        print("  >",V)
+        break
